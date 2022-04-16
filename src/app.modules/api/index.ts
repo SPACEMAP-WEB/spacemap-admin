0cc6d87dbc742eff5e128bad08f2237b8400fc15
Space Map Server <spacemap@server>
2022-04-16 08:31:37 +0000

    feature/loginTs : api모듈 타입스크립트 적용

--- src/app.modules/api/index.ts
+++ src/app.modules/api/index.ts
@@ -1,96 +1,90 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosInstance } from 'axios'
 import { notification } from 'antd'
-import { qs } from 'app.modules/util'
 import { API_GET_ACCESSTOKEN, API_GET_TOKENS, API_LOGIN } from 'app.modules/keyFactory'
 
 axios.defaults.withCredentials = true
 
+type ApiData = {
+  headers?: object
+  url: string
+  method: 'GET' | 'PUT' | 'PATCH' | 'DELETE' | 'POST'
+  data?: object | null
+}
+
 class API {
   // eslint-disable-next-line prettier/prettier
-  private readonly apiUrl: string
-  private readonly ver: string
+  private readonly requestApi: AxiosInstance
 
   constructor() {
-    this.apiUrl = process.env.SPACEMAP_ADMIN_API_URI
-    this.ver = ''
+    this.requestApi = axios.create({ baseURL: process.env.SPACEMAP_ADMIN_API_URI })
   }
 
-  async CALL({ headers = {}, url = '', method, data = null }) {
+  async Fetch({ headers = {}, url = '', method, data = null }: ApiData) {
+    return await this.requestApi({
+      method,
+      data,
+      url,
+      headers: {
+        ...headers,
+      },
+    })
+  }
+
+  async CALL({ headers = {}, url = '', method, data = null }: ApiData) {
+    const config = { headers, url, method, data }
     try {
-      const response: any = await axios({
-        method,
-        data,
-        url: this.apiUrl + url,
-        headers: {
-          ...headers,
-        },
-      })
+      const response = this.Fetch(config)
 
       return response
     } catch (error) {
-      const { message, status } = error.response.data
+      const axiosError = error as AxiosError
+      const { message, status } = axiosError.response?.data
 
       if (url !== API_LOGIN && url !== API_GET_TOKENS && message !== 'TokenExpiredError') {
         notification.error({
           message: 'error',
-          description: message.length > 0 ? message : error.toString(),
+          description: message.length > 0 ? message : axiosError.toString(),
         })
       }
 
       if (status === 401 && message === 'TokenExpiredError') {
-        await axios({
-          method: 'GET',
-          url: this.apiUrl + API_GET_ACCESSTOKEN,
-          headers: {
-            ...headers,
-          },
-        })
+        this.Fetch({ ...config, method: 'GET', url: API_GET_ACCESSTOKEN })
 
-        const response: any = await axios({
-          method,
-          data,
-          url: this.apiUrl + url,
-          headers: {
-            ...headers,
-          },
-        })
+        const response = this.Fetch({ headers, url, method, data })
         return response
       }
       throw error
     }
   }
 
-  GET(urlData) {
-    if (typeof urlData === 'object') {
-      urlData = urlData.url + qs.stringURL(urlData.data)
-    }
+  GET(urlData: string) {
     return this.CALL({
       method: 'GET',
-      url: this.ver + urlData,
+      url: urlData,
     })
   }
 
-  POST(params) {
+  POST(params: ApiData) {
     return this.CALL({
       ...params,
       method: 'POST',
-      url: this.ver + params.url,
+      url: params.url,
     })
   }
 
-  PUT(params) {
+  PUT(params: ApiData) {
     return this.CALL({
       ...params,
       method: 'PUT',
-      url: this.ver + params.url,
+      url: params.url,
     })
   }
 
-  DELETE(params) {
+  DELETE(params: ApiData) {
     return this.CALL({
       ...params,
       method: 'DELETE',
-      url: this.ver + params.url,
+      url: params.url,
     })
   }
 }
