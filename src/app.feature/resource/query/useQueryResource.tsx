819637f0f1a906af6e65ab89428d91fed0589fa2
Space Map Server <spacemap@server>
2022-04-20 14:53:57 +0000

    feat: get the detial data and set initial value

--- src/app.feature/resource/query/useQueryResource.tsx
+++ src/app.feature/resource/query/useQueryResource.tsx
@@ -3,11 +3,15 @@ import { API_RESOURCE } from 'app.modules/keyFactory'
 import { useQuery } from 'react-query'
 import { ResourceDataType } from '../types/resourceType'
 
-const requestApiGetResource = async () => {
-  const response = await api.GET(API_RESOURCE)
+const requestApiGetResource = async ({ id }: { id: string | null }) => {
+  const response = await api.GET(API_RESOURCE + (id ? `/${id}` : ''))
   return response.data.data
 }
 
 export const useQueryGetResource = () => {
-  return useQuery<ResourceDataType[]>([API_RESOURCE], () => requestApiGetResource())
+  return useQuery<ResourceDataType[]>([API_RESOURCE], () => requestApiGetResource({ id: null }))
+}
+
+export const useQueryGetResourceDetail = (id: string) => {
+  return useQuery<ResourceDataType>([API_RESOURCE, id], () => requestApiGetResource({ id }))
 }
